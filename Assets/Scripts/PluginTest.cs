f1c9cb3af221cd34a86e285348781959c81dc54f
YangHun <pitahano@gmail.com>
2020-04-20 19:28:10 +0900

    Import local notification library, custum AAR library

---
+++ Assets/Scripts/PluginTest.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class PluginTest : MonoBehaviour
+{
+    public Text text;
+
+
+    private void Start()
+    {
+        var plugin = new AndroidJavaClass("com.eg.myplugin.PluginClass");
+        text.text = plugin.CallStatic<string>("UnityCall", "testcall");
+
+        long t = 1000;
+
+        LocalNotification.SendNotification(1, t, "Send Notification Test Call!", "Yee", Color.white, true, true, true, "app_icon", null, "default");
+    }
+
+
+    public void CallFromNative(string str)
+    {
+        text.text = str;
+    }
+}
